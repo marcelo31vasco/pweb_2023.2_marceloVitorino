b77d28cc374269bc7a09438d7b1536bff0f03fa7
marcelo31vasco <macelo17@gmail.com>
2024-02-20 01:44:16 -0300

    V2_ex2

---
+++ u1_exercicio2/src/main/java/br/com/marcelo/cadpessoas_marcelo/crontrollers/PessoaController.java
@@ -0,0 +1,5 @@
+package br.com.marcelo.cadpessoas_marcelo.crontrollers;
+
+public class PessoaController {
+    
+}
---
+++ u1_exercicio2/src/main/java/br/com/marcelo/cadpessoas_marcelo/model/Pessoa.java
@@ -0,0 +1,5 @@
+package br.com.marcelo.cadpessoas_marcelo.model;
+
+public class Pessoa {
+    
+}
