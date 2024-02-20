114d2f88e0de15ebccbc005ca7adebe419430b5a
marcelo31vasco <macelo17@gmail.com>
2024-02-19 23:39:22 -0300

    V1_Ex2

---
+++ u1_exercicio2/src/main/java/br/com/marcelo/cadpessoas_marcelo/CadpessoasMarceloApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.cadpessoas_marcelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasMarceloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasMarceloApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio2/src/test/java/br/com/marcelo/cadpessoas_marcelo/CadpessoasMarceloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.cadpessoas_marcelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasMarceloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
